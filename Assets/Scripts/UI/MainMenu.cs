4bab1d4e034b113dea3b388b9479a1673a098a92
paleonluna <paleonluna@gmail.com>
2019-09-21 19:06:29 -0400

    cleaned up main menu, made all fields data driven in main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,16 +6,18 @@ using SimpleJSON;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Text _highScore = null;
-    [SerializeField] private Text _title = null;
-    [SerializeField] private Text _version = null;
-
+    [SerializeField] private Text _titleText;
+    [SerializeField] private Text _highScoreText;
+    [SerializeField] private Text _highScoreValue;
+    [SerializeField] private Text _versionText;
+    [SerializeField] private Text _versionNumber;
+    [SerializeField] private Text _startButtonText;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Going to use JSON data and not player prefs for everything in the main menu
-        //_highScore.text = PlayerPrefs.HasKey("highScore") ? PlayerPrefs.GetInt("highScore").ToString() : 0.ToString();
+        //Read in JSON data for main menu
+        if(_highScoreText) _highScoreText.text = PlayerPrefs.HasKey("highScore") ? PlayerPrefs.GetInt("highScore").ToString() : 0.ToString();
         UpdateMainMenuData();
     }
 
@@ -30,9 +32,12 @@ public class MainMenu : MonoBehaviour
         }
         else
         {
-            _version.text = mainMenuData["HighScore"].ToString();
-            _highScore.text = mainMenuData["Version"].ToString();
-            _title.text = mainMenuData["Title"].ToString();
+            if (_titleText) _titleText.text = mainMenuData["TitleText"].Value;
+            if (_highScoreText) _highScoreText.text = mainMenuData["HighScoreText"].Value;
+            if (_versionText) _versionText.text = mainMenuData["VersionText"].Value;
+            if(_versionNumber) _versionNumber.text = mainMenuData["VersionNumber"].Value;
+            if (_startButtonText) _startButtonText.text = mainMenuData["VersionNumber"].Value;
+
         }
     }
 
