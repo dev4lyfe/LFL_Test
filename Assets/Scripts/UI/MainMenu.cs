de2482da6139938df2bf38e5111bc1d663214451
paleonluna <paleonluna@gmail.com>
2019-09-21 18:29:10 -0400

    added in SimpleJSON and reading in JSON values to MainMenu.cs

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using SimpleJSON;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Text _highScore = null;
+    [SerializeField] private Text _title = null;
+    [SerializeField] private Text _version = null;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        _highScore.text = PlayerPrefs.HasKey("highScore") ? PlayerPrefs.GetInt("highScore").ToString() : 0.ToString();
+        //Going to use JSON data and not player prefs for everything in the main menu
+        //_highScore.text = PlayerPrefs.HasKey("highScore") ? PlayerPrefs.GetInt("highScore").ToString() : 0.ToString();
+        UpdateMainMenuData();
+    }
+
+    void UpdateMainMenuData()
+    {
+        object JSONobj = Resources.Load("GameJSONData/MainMenuJSON");
+        var mainMenuData = JSON.Parse(JSONobj.ToString());
+
+        if (mainMenuData == null)
+        {
+            Debug.LogWarning("JSON data for the main menu has not been assigned");
+        }
+        else
+        {
+            _version.text = mainMenuData["HighScore"].ToString();
+            _highScore.text = mainMenuData["Version"].ToString();
+            _title.text = mainMenuData["Title"].ToString();
+        }
     }
 
     // Update is called once per frame
---
+++ README.md
@@ -0,0 +1,5 @@
+The first change I made was importing SimpleJSON.
+
+Then I created a folder in Resources called GameJSONData and added a file called MainMenuJSON to that.
+
+I modified MainMenu.cs to read JSON instead of using player prefs, then assigned the values from the JSON to UI text objects.
